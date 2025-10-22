a37187caf7c7cb6ff973e5ec17765fda65f64143
rharutyunyan <rado9408@gmail.com>
2025-10-22 16:56:44 +0400

    chore: fix tests configuration

--- jest.preset.js
+++ jest.preset.js
@@ -2,6 +2,9 @@ const nxPreset = require('@nx/jest/preset').default;
 
 module.exports = { 
   ...nxPreset,
+  transform: {
+    '^.+\\.[tj]s$': ['@swc/jest'],
+  },
   coverageReporters: ['text', 'html', 'lcov'],
   collectCoverageFrom: [
     'src/**/*.{js,ts}',
